6337f5c3698ed94090fa60e6a8bf59246f4ef47a
hjinha18 <75845861+hjinha18@users.noreply.github.com>
2021-05-30 00:45:40 +0900

    Update Navigation_priority_queue.cpp

--- Navigation/Navigation_priority_queue.cpp
+++ Navigation/Navigation_priority_queue.cpp
@@ -4,30 +4,31 @@
 #include <string>
 using namespace std;
 
-struct comp {
-	bool operator()(pair<int, int> a, pair<int, int> b) {
-		if (a.second == b.second) return a.first > b.first;
-		else return a.second > b.second;
-	}
-};
-
 string areas[1000000]; // 지역 이름을 저장하기 위한 배열
 int distances[1000000]; // 최단거리를 저장하기 위한 배열
 int predecessor[1000000]; // 경로를 추적하기 위한 배열
 vector<vector<pair<int, int>>> roads(1000000); // 도로를 저장하기 위한 adjacency list
 
+struct comp { // 거리 기준 정렬, 거리가 같다면 지역번호 기준 정렬
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		if (a.second == b.second)
+			return a.first > b.first;
+		return a.second > b.second;
+	}
+};
 
 void minDist(int sourceNum, int destNum) {
 	if (areas[sourceNum] == "" || areas[destNum] == "") { // 침수된 지역이 포함되었으면
 		cout << "None\n";
 		return;
 	}
-		
+
 	int treeNum = 0;
-	for (int i = 100000; i < 1000000; i++) distances[i] = 1000000; // 최대 거리로 배열 초기화
+	for (int i = 100000; i < 1000000; i++) // 배열 초기화
+		distances[i] = 1000000;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, comp> fringeQ; // fringe node를 저장하기 위한 우선순위 큐
 
-	fringeQ.push({ sourceNum,0 });
+	fringeQ.push({ sourceNum, 0 });
 	distances[sourceNum] = 0;
 
 	int curNum, distance;
@@ -36,27 +37,34 @@ void minDist(int sourceNum, int destNum) {
 		distance = fringeQ.top().second; // 거리
 		fringeQ.pop();
 
-		if (distance > distances[curNum]) // 이미 확인했고, 더 짧다면 무시
-			continue; 
+		if (distances[curNum] < distance) // 이미 확인했고, 더 짧다면 무시
+			continue;
 		treeNum++; // 무시되지 않는다면 fringe -> tree 노드가 됨
 
 		if (curNum == destNum) // 도착 노드가 tree 됐다면 종료
 			break;
 
+		//int next, nextDistance;
+		//for (pair<int, int> road : roads[curNum]) { // 연결된 지역들 탐색
+		//	next = road.first;
+		//	nextDistance = road.second + distance;
+		//	if (nextDistance < distances[next]) { // 거리 갱신
+		//		distances[next] = nextDistance;
+		//		fringeQ.push({ next, nextDistance });}
+		//}
 		int next, nextDistance;
 		for (int i = 0; i < roads[curNum].size(); i++) { // 연결된 지역들 탐색
 			next = roads[curNum][i].first; // 연결된 지역 번호
-			nextDistance = roads[curNum][i].second + distance;
-			if (distances[next] > nextDistance) { // 거리 갱신
+			nextDistance = distance + roads[curNum][i].second;
+			if (nextDistance < distances[next]) { // 거리 갱신
+				fringeQ.push({ next, nextDistance }); // fringe 큐에 넣기
 				distances[next] = nextDistance;
-				fringeQ.push({ next, nextDistance });
 			}
 		}
 	}
 
 	if (distances[destNum] > 100000) cout << "None\n";
-	else
-		cout << treeNum << " " << distances[destNum] << " " << areas[sourceNum] << " " << areas[destNum] << "\n";
+	else cout << treeNum << " " << distances[destNum] << " " << areas[sourceNum] << " " << areas[destNum] << "\n";
 }
 
 void minRoute(int sourceNum, int destNum) {
@@ -66,10 +74,11 @@ void minRoute(int sourceNum, int destNum) {
 	}
 
 	int treeNum = 0;
-	for (int i = 0; i < 1000000; i++) distances[i] = 1000000; // 최대 거리로 배열 초기화
+	for (int i = 100000; i < 1000000; i++) // 배열 초기화
+		distances[i] = 1000000;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, comp> fringeQ; // fringe node를 저장하기 위한 우선순위 큐
 
-	fringeQ.push({ sourceNum,0 });
+	fringeQ.push({ sourceNum, 0 });
 	distances[sourceNum] = 0;
 
 	int curNum, distance;
@@ -78,21 +87,20 @@ void minRoute(int sourceNum, int destNum) {
 		distance = fringeQ.top().second; // 거리
 		fringeQ.pop();
 
-		if (distance > distances[curNum]) // 이미 확인했고, 더 짧다면 무시
+		if (distances[curNum] < distance) // 이미 확인했고, 더 짧다면 무시
 			continue;
 		treeNum++; // 무시되지 않는다면 fringe -> tree 노드가 됨
-
 		if (curNum == destNum) // 도착 노드가 tree 됐다면 종료
 			break;
 
 		int next, nextDistance;
 		for (int i = 0; i < roads[curNum].size(); i++) { // 연결된 지역들 탐색
 			next = roads[curNum][i].first; // 연결된 지역 번호
-			nextDistance = roads[curNum][i].second + distance;
-			if (distances[next] > nextDistance) { // 거리 갱신
+			nextDistance = distance + roads[curNum][i].second;
+			if (nextDistance < distances[next]) { // 거리 갱신
+				fringeQ.push({ next, nextDistance }); // fringe 큐에 넣기
 				distances[next] = nextDistance;
-				fringeQ.push({ next, nextDistance });
-				predecessor[next] = curNum;
+				predecessor[next] = curNum; // 경로 추적을 위해 이전 노드 정보를 저장
 			}
 		}
 	}
@@ -113,7 +121,7 @@ int main() {
 	int nodeNum, edgeNum, questionNum, areaNum, flooded;
 	string areaName;
 	cin >> nodeNum >> edgeNum >> questionNum;
-	
+
 	for (int i = 0; i < nodeNum; i++) {
 		cin >> areaNum >> areaName >> flooded;
 		if (flooded == 0) {
@@ -125,9 +133,9 @@ int main() {
 	for (int i = 0; i < edgeNum; i++) {
 		cin >> areaNum1 >> areaNum2 >> distance;
 		if (areas[areaNum1] != "" && areas[areaNum2] != "") { //침수되지 않은 지역만 넣음
-			roads[areaNum1].push_back({ areaNum2,distance });
 			//방향이 없으니까 양쪽에 다 넣어야 한다.
-			roads[areaNum2].push_back({ areaNum1,distance });
+			roads[areaNum1].push_back({ areaNum2, distance });
+			roads[areaNum2].push_back({ areaNum1, distance });
 		}
 	}
 
